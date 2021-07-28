2c989517fb1d7504a2b46f437d1f28859c0cf63f
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-28 17:37:59 +0200

    add functionality to close menu after clicking anywhere on document body

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const menuElement = document.querySelector(".menu");
 
 const exitBtn = document.querySelector(".close-btn");
 
+const body = document.querySelector("body");
+
+let isOpen = false;
+
 hamburgerMenu.addEventListener("click", function () {
   menuElement.classList.add("show-menu");
 });
@@ -11,3 +15,19 @@ hamburgerMenu.addEventListener("click", function () {
 exitBtn.addEventListener("click", function () {
   menuElement.classList.remove("show-menu");
 });
+
+// body.addEventListener("click", function (event) {
+//   if (event.target.classList !== "menu") {
+//     menuElement.classList.remove("show-menu");
+//   }
+// });
+
+document.addEventListener(
+  "click",
+  function () {
+    if (menuElement.classList.contains("show-menu")) {
+      menuElement.classList.remove("show-menu");
+    }
+  },
+  true
+);
