449c6f688ce43d31cea74934d96af057711f5535
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-09-13 13:16:08 +0200

    delete unnecessary commented code

--- main.js
+++ main.js
@@ -32,36 +32,6 @@ document.addEventListener(
   true
 );
 
-// const nextBtn = document.querySelector(".arrow__right");
-// const prevBtn = document.querySelector(".arrow__left");
-// const currentPicture = document.querySelector(".active");
-
-// nextBtn.addEventListener("click", () => {
-//   console.log("next button added!");
-// });
-
-// prevBtn.addEventListener("click", () => {
-//   console.log("previous button added!");
-// });
-
-// nextBtn.addEventListener("click", showNextPicture);
-
-// function showNextPicture() {
-//   if (currentPicture.classList.contains("active")) {
-//     currentPicture.classList.remove("active");
-//     currentPicture.nextElementSibling.classList.add("active");
-//   }
-// }
-
-// prevBtn.addEventListener("click", showPrevPicture);
-
-// function showPrevPicture() {
-//   if (currentPicture.classList.contains("active")) {
-//     currentPicture.classList.remove("active");
-//     currentPicture.previousElementSibling.classList.add("active");
-//   }
-// }
-
 document.querySelector(".image").classList.add("active");
 
 const nextBtn = document.querySelector(".arrow__right");
@@ -110,9 +80,3 @@ function nextImage(direction) {
   }
   slides[index].classList.add("visible");
 }
-
-// nextBtn.addEventListener("click", () => {
-//   const currentActive = document.querySelector(".active");
-//   currentActive.classList.remove("active");
-//   currentActive.nextElementSibling.classList.add("active");
-// });
